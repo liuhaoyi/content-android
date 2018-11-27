b4de2405035ea41f40528840a4d9de0aca8aad8f
liuhy <25436136@qq.com>
2018-11-27 20:37:53 +0800

    完善webview设置参数。

--- app/src/main/java/com/example/liuhy/myapp2/MainActivity.java
+++ app/src/main/java/com/example/liuhy/myapp2/MainActivity.java
@@ -24,59 +24,127 @@ import cn.sharesdk.js.ShareSDKUtils;
 import cn.sharesdk.onekeyshare.OnekeyShare;
 
 
-public class MainActivity extends Activity {
+//public class MainActivity extends Activity {
 
+//    protected void onCreate(Bundle savedInstanceState) {
+//        super.onCreate(savedInstanceState);
+//
+//        WebView webView = new WebView(this);
+//        WebViewClient wvClient = new WebViewClient();
+//        webView.setWebViewClient(wvClient);
+//        webView.setWebChromeClient(new WebChromeClient() {
+//            public boolean onJsAlert(WebView view, String url, String message,
+//                                     JsResult result) {
+//                return super.onJsAlert(view, url, message, result);
+//            }
+//        });
+//
+//        // you must call the following line after the webviewclient is set into the webview
+//        ShareSDKUtils.prepare(webView, wvClient);
+//        setContentView(webView);
+////        webView.loadUrl("file:///android_asset/Sample.html");
+//        webView.loadUrl("http://192.168.1.104:8001/login");
+//    }
+//}
+
+
+public class MainActivity extends AppCompatActivity {
+
+    WebView webView = null;
+    private long exitTime = 0;
+
+    @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        requestWindowFeature(Window.FEATURE_NO_TITLE);
+        setContentView(R.layout.activity_main);
+        webView = (WebView) this.findViewById(R.id.webView);
+        initWebView();
+        webView.loadUrl("http://192.168.170.3:8000/login");
+//        webView.loadUrl("http://192.168.2.116:8000/login");
 
-        WebView webView = new WebView(this);
-        WebViewClient wvClient = new WebViewClient();
-        webView.setWebViewClient(wvClient);
+
+    }
+
+
+    private void initWebView() {
+
+        WebViewClient webviewClient =  new WebViewClient() {
+            @Override
+            public void onReceivedError(WebView view, int errorCode, String description, String failingUrl) {
+                super.onReceivedError(view, errorCode, description, failingUrl);
+                //6.0以下执行
+//                Log.i(TAG, "onReceivedError: ------->errorCode" + errorCode + ":" + description);
+                //网络未连接
+                showErrorPage();
+            }
+
+            //处理网页加载失败时
+            @Override
+            public void onReceivedError(WebView view, WebResourceRequest request, WebResourceError error) {
+                super.onReceivedError(view, request, error);
+                //6.0以上执行
+//                Log.i(TAG, "onReceivedError: ");
+                showErrorPage();//显示错误页面
+            }
+        };
+
+
+
+        //加载需要显示的网页
+//        webView.loadUrl("file:///android_asset/Sample.html");
+        WebSettings mWebSettings = webView.getSettings();
+        mWebSettings.setJavaScriptEnabled(true);    //允许加载javascript
+        mWebSettings.setDomStorageEnabled(true);
+        mWebSettings.setAppCacheMaxSize(1024*1024*8);
+        mWebSettings.setAllowFileAccess(true);
+        mWebSettings.setAppCacheEnabled(true);
+        webView.setWebViewClient(webviewClient);
         webView.setWebChromeClient(new WebChromeClient() {
+            @Override
+            public void onProgressChanged(WebView view, int newProgress) {
+//                Log.i(TAG, "onProgressChanged:----------->" + newProgress);
+//                if (newProgress == 100) {
+//                    loadingLayout.setVisibility(View.GONE);
+//                }
+            }
+
+
+            @Override
+            public void onReceivedTitle(WebView view, String title) {
+                super.onReceivedTitle(view, title);
+//                Log.i(TAG, "onReceivedTitle:title ------>" + title);
+//                if (title.contains("404")){
+//                    showErrorPage();
+//                }
+            }
             public boolean onJsAlert(WebView view, String url, String message,
                                      JsResult result) {
                 return super.onJsAlert(view, url, message, result);
             }
         });
 
-        // you must call the following line after the webviewclient is set into the webview
-        ShareSDKUtils.prepare(webView, wvClient);
-        setContentView(webView);
-//        webView.loadUrl("file:///android_asset/Sample.html");
-        webView.loadUrl("http://192.168.1.104:8000/");
+        ShareSDKUtils.prepare(webView, webviewClient);
+
+//        webParentView = (RelativeLayout) webView.getParent(); //获取父容器
     }
-}
 
 
-//public class MainActivity extends AppCompatActivity {
-//
-//    WebView webView = null;
-//    private long exitTime = 0;
-//
-//    @Override
-//    protected void onCreate(Bundle savedInstanceState) {
-//        super.onCreate(savedInstanceState);
-//        requestWindowFeature(Window.FEATURE_NO_TITLE);
-//        setContentView(R.layout.activity_main);
-//
-//        webView = (WebView) this.findViewById(R.id.webView);
-//        webView.setScrollContainer(false);
-//        MobSDK.init(this, "28b635de569a6", "6a2b2af1161ede7f37919462eec949c3");
-//
-////        textview.loadUrl("https://segmentfault.com/q/1010000009509118?_ea=1958906");
-//        initWebView();
-////        ShareSDK.in(this);
-//
-//
-//        Button btn = (Button)this.findViewById(R.id.button);
-//        btn.setOnClickListener(new View.OnClickListener() {
-//            @Override
-//            public void onClick(View v) {
-//                MainActivity.this.showShare();
-//            }
-//        });
-//    }
-//
+    private void showErrorPage() {
+//        webParentView.removeAllViews(); //移除加载网页错误时，默认的提示信息
+//        LinearLayout.LayoutParams layoutParams = new LinearLayout.LayoutParams(LinearLayout.LayoutParams.MATCH_PARENT, LinearLayout.LayoutParams.MATCH_PARENT);
+//        webParentView.addView(mErrorView, 0, layoutParams); //添加自定义的错误提示的View
+    }
+
+    /***
+     * 显示加载失败时自定义的网页
+     */
+    private void initErrorPage() {
+//        if (mErrorView == null) {
+//            mErrorView = View.inflate(this, R.layout.layout_load_error, null);
+//        }
+    }
+
 //    private void showShare() {
 //        OnekeyShare oks = new OnekeyShare();
 //        //关闭sso授权
@@ -103,98 +171,21 @@ public class MainActivity extends Activity {
 //// 启动分享GUI
 //        oks.show(this);
 //    }
-//    private void initWebView() {
-//
-//        WebViewClient webviewClient =  new WebViewClient() {
-//            @Override
-//            public void onReceivedError(WebView view, int errorCode, String description, String failingUrl) {
-//                super.onReceivedError(view, errorCode, description, failingUrl);
-//                //6.0以下执行
-////                Log.i(TAG, "onReceivedError: ------->errorCode" + errorCode + ":" + description);
-//                //网络未连接
-//                showErrorPage();
-//            }
-//
-//            //处理网页加载失败时
-//            @Override
-//            public void onReceivedError(WebView view, WebResourceRequest request, WebResourceError error) {
-//                super.onReceivedError(view, request, error);
-//                //6.0以上执行
-////                Log.i(TAG, "onReceivedError: ");
-//                showErrorPage();//显示错误页面
-//            }
-//        };
-//
-//        ShareSDKUtils.prepare(webView, webviewClient);
-//
-//        //加载需要显示的网页
-////        webView.loadUrl("http://192.168.1.104:8000/login");
-//        webView.loadUrl("file:///android_asset/Sample.html");
-//        WebSettings mWebSettings = webView.getSettings();
-//        mWebSettings.setJavaScriptEnabled(true);    //允许加载javascript
-//        mWebSettings.setSupportZoom(true);          //允许缩放
-//        mWebSettings.setBuiltInZoomControls(true);  //原网页基础上缩放
-//        mWebSettings.setUseWideViewPort(true);      //任意比例缩放
-//        webView.setWebViewClient(webviewClient);
-//
-//
-//
-//        webView.setWebChromeClient(new WebChromeClient() {
-//            @Override
-//            public void onProgressChanged(WebView view, int newProgress) {
-////                Log.i(TAG, "onProgressChanged:----------->" + newProgress);
-////                if (newProgress == 100) {
-////                    loadingLayout.setVisibility(View.GONE);
-////                }
-//            }
-//
-//
-//            @Override
-//            public void onReceivedTitle(WebView view, String title) {
-//                super.onReceivedTitle(view, title);
-////                Log.i(TAG, "onReceivedTitle:title ------>" + title);
-////                if (title.contains("404")){
-////                    showErrorPage();
-////                }
-//            }
-//            public boolean onJsAlert(WebView view, String url, String message,
-//                                     JsResult result) {
-//                return super.onJsAlert(view, url, message, result);
-//            }
-//        });
-//
-////        webParentView = (RelativeLayout) webView.getParent(); //获取父容器
-//    }
-//
-//
-//    private void showErrorPage() {
-////        webParentView.removeAllViews(); //移除加载网页错误时，默认的提示信息
-////        LinearLayout.LayoutParams layoutParams = new LinearLayout.LayoutParams(LinearLayout.LayoutParams.MATCH_PARENT, LinearLayout.LayoutParams.MATCH_PARENT);
-////        webParentView.addView(mErrorView, 0, layoutParams); //添加自定义的错误提示的View
-//    }
-//
-//    /***
-//     * 显示加载失败时自定义的网页
-//     */
-//    private void initErrorPage() {
-////        if (mErrorView == null) {
-////            mErrorView = View.inflate(this, R.layout.layout_load_error, null);
-////        }
-//    }
-//    @Override
-//    public boolean onKeyDown(int keyCode, KeyEvent event) {
-//        if(keyCode == KeyEvent.KEYCODE_BACK && event.getAction() == KeyEvent.ACTION_DOWN){
-//            if((System.currentTimeMillis()-exitTime) > 2000){
-//                Toast.makeText(getApplicationContext(), "再按一次退出程序", Toast.LENGTH_SHORT).show();
-//                exitTime = System.currentTimeMillis();
-//            } else {
-//                finish();
-//                System.exit(0);
-//            }
-//            return true;
-//        }
-//        return super.onKeyDown(keyCode, event);
-//    }
-//
-//
-//}
+
+    @Override
+    public boolean onKeyDown(int keyCode, KeyEvent event) {
+        if(keyCode == KeyEvent.KEYCODE_BACK && event.getAction() == KeyEvent.ACTION_DOWN){
+            if((System.currentTimeMillis()-exitTime) > 2000){
+                Toast.makeText(getApplicationContext(), "再按一次退出程序", Toast.LENGTH_SHORT).show();
+                exitTime = System.currentTimeMillis();
+            } else {
+                finish();
+                System.exit(0);
+            }
+            return true;
+        }
+        return super.onKeyDown(keyCode, event);
+    }
+
+
+}
